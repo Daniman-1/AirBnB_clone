7cafd55674c27bbc7a8b701d41ff8b91016e4bc9
Noahxyzs <105595182+Noahxyzs@users.noreply.github.com>
2023-05-11 21:08:37 +0300

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
+"""Defines BaseModel class."""
 import models
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
-    """Represents the BaseModel of the HBnB project."""
+    """Represents BaseModel of the HBnB project."""
 
     def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel.
